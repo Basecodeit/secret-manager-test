bf9c0708e0a89fef53a142f13e53ff61d9ed6c3a
scastelliBit <58699625+scastelliBit@users.noreply.github.com>
2021-08-23 17:26:10 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://aws.amazon.com/developers/getting-started/nodejs/
+
+const layer = require('lambdaLayerTest');
+
+exports.handler = async (event, context) => {
+    console.log(event.headers.Authorization);
+    console.log(process.env.environment);
+    console.log(context);
+    
+   await layer.auth(event.headers.Authorization.split(' ')[1], context.functionName);
+  
+ return {
+        statusCode: 200,
+        body: "Success"
+    };
+
+};
